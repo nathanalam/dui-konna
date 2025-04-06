fca52ea1561a67923dca14799862c1346af6bde5
Nathan Alam <nathan.alam@scale.com>
2025-04-06 03:42:50 -0400

    Update static Next assets

--- next.config.mjs
+++ next.config.mjs
@@ -31,6 +31,13 @@ const nextConfig = {
   trailingSlash: true,
   basePath: '/dui-konna',
   assetPrefix: '/dui-konna/',
+  distDir: 'out',
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.output.publicPath = '/dui-konna/';
+    }
+    return config;
+  },
 }
 
 if (userConfig) {
